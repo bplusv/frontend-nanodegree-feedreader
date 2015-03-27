9bacd9e6aba2ae334a28684f3cdbef4f1aeb6e7e
Luis Salazar <bp.lusv@gmail.com>
2015-03-26 21:48:17 -0600

    Add RSS Feeds test: each feed has an url.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,6 +31,12 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('each feed has an url', function() {
+            allFeeds.forEach(function(item) {
+              expect(item.url).toBeDefined();
+              expect(item.url.trim().length).not.toBe(0);
+            });
+         });
 
 
         /* TODO: Write a test that loops through each feed

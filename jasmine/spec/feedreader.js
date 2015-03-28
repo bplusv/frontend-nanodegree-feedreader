cd24d7af5681ede583fc4ab44adc6adde1a9504d
Luis Salazar <bp.lusv@gmail.com>
2015-03-28 12:25:42 -0600

    Add Initial Entries suite: check loadFeed() loads entries correctly.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -83,6 +83,12 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function(done) {
+        beforeEach(function(done) {
+          loadFeed(0, function() {
+            done();
+          });
+        });
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -90,6 +96,11 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test wil require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        it('should load correctly', function(done) {
+          expect($('.feed .entry').length).not.toBe(0);
+          done();
+        });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection"
 
